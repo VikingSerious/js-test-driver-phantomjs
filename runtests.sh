2d3d505ec00b1e1b120b73e9a20d378aa471cdfb
Larry Myers <larry@retrofitme.com>
2012-04-06 20:57:54 -0400

    working download of jstd jars

---
+++ runtests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ROOTDIR="$( cd "$( dirname "$0")" && pwd )"
+PHANTOMJS_HOME=$PHANTOMJS_HOME
+JSTD_VERSION=1.3.4b
+
+if [ -z "$PHANTOMJS_HOME" ]; then
+    echo "PHANTOMJS_HOME env var not set."
+    exit 1
+fi
+
+if [ ! -f "$ROOTDIR/coverage-$JSTD_VERSION.jar" ]; then
+    echo "Downloading JsTestDriver jar ..."
+    curl http://js-test-driver.googlecode.com/files/coverage-$JSTD_VERSION.jar > coverage-$JSTD_VERSION.jar
+fi
+
+if [ ! -f "$ROOTDIR/JsTestDriver-$JSTD_VERSION.jar" ]; then
+    echo "Downloading coverage jar ..."
+    curl http://js-test-driver.googlecode.com/files/JsTestDriver-$JSTD_VERSION.jar > JsTestDriver-$JSTD_VERSION.jar
+fi
+
